e308222b949afd51a6131b0ac36293b22611ae45
Shrestha Mishra <smishra358@gatech.edu>
2023-11-22 11:14:37 -0500

    added file for choropleth map

--- Flask/static/choropleth_map.js
+++ Flask/static/choropleth_map.js
@@ -1,6 +1,16 @@
 
 function choropleth_map()
 {
-    var map_container = document.getElementById('mapContainer');
+    console.log("inside the function");
+    var map_container = document.createElement('div');
+
+    var paragraph = document.createElement('p');
+    // Set the text content of the <p> element
+    paragraph.textContent = 'Additional information in the paragraph.';
+
+    // Append the text node and the <p> element to the new div
+    map_container.appendChild(paragraph);
+
+    return map_container
     
 }
