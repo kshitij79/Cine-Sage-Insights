f093abf390a1700238ac77c6d6639f0e2c4ef18d
Shrestha Mishra <smishra358@gatech.edu>
2023-11-22 16:45:20 -0500

    added d3 libraries for choropleth map

--- Flask/static/choropleth_map.js
+++ Flask/static/choropleth_map.js
@@ -1,7 +1,6 @@
 
 function choropleth_map()
 {
-    console.log("inside the function");
     var map_container = document.createElement('div');
 
     var paragraph = document.createElement('p');
@@ -10,7 +9,30 @@ function choropleth_map()
 
     // Append the text node and the <p> element to the new div
     map_container.appendChild(paragraph);
-
-    return map_container
+     // define the dimensions and margins for the graph
+     margin = ({top: 100, right: 100, bottom: 100, left: 100})
+     width = 960
+     height = 500
+ 
+     // define function to parse time in years format
+ 
+     // create scales x & y for X and Y axis and set their ranges
+     x = d3.scaleTime()
+           .range([margin.left, width - margin.right]);
+     y = d3.scaleLinear()
+           .range([height - margin.bottom, margin.top]);
+ 
+ 
+     // append svg element to the body of the page
+     // set dimensions and position of the svg element
+     var svg = d3.select("body")
+               .append("svg")
+               .attr("id", "svg1")
+               .attr("width", width)
+               .attr("height", height);
+     
+     var container = svg.append("g")
+             .attr("id", "container")
+    return svg
     
 }
